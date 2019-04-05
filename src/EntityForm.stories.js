6a406395398c5c0db9d5b01b8dc025aa1c61d0ba
myGitUser13 <vanbeethoven01@yahoo.com>
2019-04-06 02:00:20 +0300

    Just removed extra parenthesis on callback

--- src/EntityForm.stories.js
+++ src/EntityForm.stories.js
@@ -3,6 +3,4 @@ import { storiesOf } from '@storybook/react';
 import EntityForm from './EntityForm';
 
 storiesOf('EntityForm', module)
-  .add('Add Form', () => (
-    <EntityForm formType="add" schema={{name:1,permission:1}} onSubmit={function(){}} onChange={function(){}}/>
-  ));
+  .add('Add Form', () => <EntityForm formType="add" schema={{name:1,permission:1}} onSubmit={function(){}} onChange={function(){}}/>);
